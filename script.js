dcf03a9fed2873c015ee1aa9b336bd2d6f9a9a10
Donkor Jeffery <donkorjeffery109@gmail.com>
2025-10-17 05:51:09 +0000

    Version 2

--- script.js
+++ script.js
@@ -78,3 +78,9 @@ revealElements.forEach(element => {
 window.addEventListener('scroll', revealOnScroll);
 // Initial check for elements in view
 revealOnScroll(); 
+
+
+
+// GIT TUTORIAL
+
+console.log('version 2')
